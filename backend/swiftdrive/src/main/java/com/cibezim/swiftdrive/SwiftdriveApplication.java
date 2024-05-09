7bbbb70320a7c207781bb27abba1e9fbb812ae22
Chukwuma Ibezim <123107253+cibezim@users.noreply.github.com>
2024-05-09 19:18:23 -0400

    feat: adds backend folder with spring initializar

---
+++ backend/swiftdrive/src/main/java/com/cibezim/swiftdrive/SwiftdriveApplication.java
@@ -0,0 +1,13 @@
+package com.cibezim.swiftdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftdriveApplication.class, args);
+	}
+
+}
---
+++ backend/swiftdrive/src/test/java/com/cibezim/swiftdrive/SwiftdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibezim.swiftdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
